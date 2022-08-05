5f60895e9c661b6692fe5efb5eff96f797fa5e9c
AdityaShastri14 <110630424+AdityaShastri14@users.noreply.github.com>
2022-08-05 14:46:36 -0400

    Add files via upload

---
+++ MAD3144S22LABTEST1/file.js
@@ -0,0 +1,13 @@
+
+        document.write(sum(1,100));
+        function sum(a,b){
+        const start = a;
+        const end = b;
+        var current;
+        var sum = 0;
+        for (var i=a; i<=b; i++ ){
+            sum = sum+i;
+            }
+        return sum;
+          }
+        console.log(sum);
